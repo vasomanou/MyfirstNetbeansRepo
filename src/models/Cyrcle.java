c8075a4f2972d4c20644f0a483d17f4cd6603a4a
vasomanou <manou.e.vaso@gmail.com>
2022-05-04 20:34:17 +0300

    addingCyrcle class

---
+++ src/models/Cyrcle.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author vaso
+ */
+public class Cyrcle {
+    private int x;
+    private int y;
+    private int z;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+    
+    
+    
+}
